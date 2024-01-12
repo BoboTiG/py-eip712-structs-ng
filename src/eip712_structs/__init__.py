0ec701606d694edaad39c8cd9e93a3df9756324d
Mickaël Schoentgen <contact@tiger-222.fr>
2024-01-12 01:00:41 +0100

    chore: Bump the version

--- src/eip712_structs/__init__.py
+++ src/eip712_structs/__init__.py
@@ -2,7 +2,7 @@ from eip712_structs.domain_separator import make_domain
 from eip712_structs.struct import EIP712Struct
 from eip712_structs.types import Address, Array, Boolean, Bytes, Int, String, Uint
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __all__ = (
     "make_domain",
     "EIP712Struct",
